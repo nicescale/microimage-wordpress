dda77386f72cfff887bad6b9b53f3e55c4cdefd9
shellrock <hanwoody@gmail.com>
2016-01-29 20:39:50 +0800

    build from aws

--- build.sh
+++ build.sh
@@ -8,16 +8,14 @@ export IMAGE=wordpress
 export TAGS="4 4.3"
 export BASE_IMAGE=microimages/php-apache
 
-docker pull $REG_URL/$BASE_IMAGE
+docker pull $BASE_IMAGE
 
-docker tag -f $REG_URL/$BASE_IMAGE $BASE_IMAGE
-
-docker build -t $REG_URL/microimages/$IMAGE .
+docker build -t microimages/$IMAGE .
 
 #./test.sh
 
 for t in $TAGS; do
-  docker tag -f $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$t
+  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
 done
 
-docker push $REG_URL/microimages/$IMAGE
+docker push microimages/$IMAGE
